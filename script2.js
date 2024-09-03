dc3b11ccdc22fbcaf93ac5c7969911caaa0fc5c8
dlllonb <bassdillon10@gmail.com>
2024-09-03 17:01:39 -0400

    homepage new work

---
+++ script2.js
@@ -0,0 +1,43 @@
+
+
+// Function that controls scroll down from arrow on home page
+document.addEventListener("DOMContentLoaded", function() {
+    const downScroll = document.getElementById("home-learn-more");
+    const scrollAmount = 995;
+
+    function scrollDown() {
+        window.scrollBy({
+            top: scrollAmount,
+            behavior: "smooth"
+        });
+    }
+
+    downScroll.addEventListener("click", scrollDown);
+});
+
+// Function that controls the fade in between pages
+window.addEventListener('load', function() {
+    document.body.classList.remove('fade-out');
+    setTimeout(() => {
+        document.body.classList.add('fade-in');
+    }, 5); 
+});
+
+// Function that controls the up-sliding of new elements to the page
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target); 
+            }
+        });
+    }, {
+        threshold: 0.05 
+    });
+
+    const elements = document.querySelectorAll('.hidden');
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
