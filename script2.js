46eb131275a87288d762605a744c48916908383c
dlllonb <bassdillon10@gmail.com>
2024-12-28 16:18:57 -0500

    new homepage

--- script2.js
+++ script2.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function scrollDownOnePage() {
         window.scrollBy({
-            top: window.innerHeight,
+            top: window.innerHeight + 80,
             behavior: "smooth" 
         });
     }
