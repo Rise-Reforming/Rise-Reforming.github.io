cab3ffc6133bebe60e2f3bb0896739e16047e4a8
dlllonb <bassdillon10@gmail.com>
2025-01-03 10:21:48 -0500

    minor change

--- script2.js
+++ script2.js
@@ -161,7 +161,7 @@ document.addEventListener("DOMContentLoaded", function() {
 // Function to make Impact title disappear smoothly
 document.addEventListener("DOMContentLoaded", function() {
     const targetText = document.getElementById("impact-title-main");
-    const scrollTriggerPoint = 150; 
+    const scrollTriggerPoint = 200; 
 
     window.addEventListener("scroll", function() {
         const scrollPosition = window.scrollY;
