8bb31df89e57cbc76f6846f096c778304eb1e5cf
dlllonb <bassdillon10@gmail.com>
2025-01-20 11:17:10 -0600

    mobile size adjustments

--- script2.js
+++ script2.js
@@ -305,3 +305,8 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+      card.classList.toggle('flipped'); // Toggle the "flipped" class on click
+    });
+  });
